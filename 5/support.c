d41da1d6ef669ac514c10dd3587621b1253e5a39
wnd01jun <changjun157@naver.com>
2025-05-17 13:13:26 +0900

    feat : support.c header file 추가

--- 5/support.c
+++ 5/support.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include "support.h"
 #include "type.h"
 
 extern char *yytext;
@@ -11,42 +12,7 @@ int syntax_err = 0;
 extern int line_no;
 int current_level = 0;
 
-A_NODE *makeNode(NODE_NAME, A_NODE *, A_NODE *, A_NODE *);
-A_NODE *makeNodeList(NODE_NAME, A_NODE *, A_NODE *);
-A_ID *makeIdentifier(char *);
-A_ID *makeDummyIdentifier();
-A_TYPE *makeType(T_KIND);
-A_SPECIFIER *makeSpecifier(A_TYPE *, S_KIND);
-A_ID *searchIdentifier(char *, A_ID *);
-A_ID *searchIdentifierAtCurrentLevel(char *, A_ID *);
-A_SPECIFIER *updateSpecifier(A_SPECIFIER *, A_TYPE *, S_KIND);
-void checkForwardReference();
-void setDefaultSpecifier(A_SPECIFIER *);
 
-A_ID *linkDeclaratorList(A_ID *,A_ID *);
-A_ID *getIdentifierDeclared (char *);
-A_TYPE *getTypeOfStructOrEnumRefIdentifier(T_KIND,char *,ID_KIND);
-A_ID *setDeclaratorInit(A_ID *,A_NODE *);
-A_ID *setDeclaratorKind(A_ID *, ID_KIND);
-A_ID *setDeclaratorType(A_ID *, A_TYPE *);
-A_ID *setDeclaratorElementType(A_ID *,A_TYPE *);
-A_ID *setDeclaratorTypeAndKind(A_ID *,A_TYPE *,ID_KIND);
-A_ID *setDeclaratorListSpecifier(A_ID *,A_SPECIFIER *);
-A_ID *setFunctionDeclaratorSpecifier(A_ID *, A_SPECIFIER *);
-A_ID *setFunctionDeclaratorBody(A_ID *, A_NODE *);
-A_ID *setParameterDeclaratorSpecifier(A_ID *, A_SPECIFIER *);
-A_ID *setStructDeclaratorListSpecifier(A_ID *, A_TYPE *);
-A_TYPE *setTypeNameSpecifier(A_TYPE *, A_SPECIFIER *);
-A_TYPE *setTypeElementType(A_TYPE *, A_TYPE *);
-A_TYPE *setTypeField(A_TYPE *,A_ID *);
-A_TYPE *setTypeExpr(A_TYPE *, A_NODE *);
-A_TYPE *setTypeAndKindOfDeclarator(A_TYPE *, ID_KIND, A_ID *);
-A_TYPE *setTypeStructOrEnumIdentifier(T_KIND, char *, ID_KIND);
-BOOLEAN isNotSameFormalParameters(A_ID *, A_ID *);
-BOOLEAN isNotSameType(A_TYPE *, A_TYPE *);
-BOOLEAN isPointerOrArrayType(A_TYPE *);
-void syntax_error();
-void initialize();
 
 A_NODE *makeNode(NODE_NAME n, A_NODE *a, A_NODE *b, A_NODE *c) {
     A_NODE *m; // A_NODE는 자식 3개를 가질 수 있는 node 보통 가운데는 identifier
